cdf581b9ae16d8f9d260a178c71c0dab35be3a9e
Salman Shahid <salmanshahidzia@gmail.com>
2024-03-22 23:02:43 +0000

    fix device issue

--- omega/base/validator.py
+++ omega/base/validator.py
@@ -330,7 +330,7 @@ class BaseValidatorNeuron(BaseNeuron):
 
         # Compute forward pass rewards, assumes uids are mutually exclusive.
         # shape: [ metagraph.n ]
-        scattered_rewards: torch.FloatTensor = self.scores.scatter(
+        scattered_rewards: torch.FloatTensor = self.scores.to(self.device).scatter(
             0, uids_tensor.to(self.device), rewards.to(self.device)
         ).to(self.device)
         bt.logging.debug(f"Scattered rewards: {rewards}")
