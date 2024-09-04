7f3bf6f9bca1adcb212552ad3b0a00e49939362d
Salman <salmanshahidzia@gmail.com>
2024-09-04 17:51:38 +0000

    hotfix for wandb_run_start

--- neurons/miner.py
+++ neurons/miner.py
@@ -60,7 +60,7 @@ class Miner(BaseMinerNeuron):
             raise ValueError("Invalid query augment")
         
         self.imagebind_v1 = ImageBind(disable_lora=True)
-        self.imagebind_v2 = ImageBind(disable_lora=False)
+        # self.imagebind_v2 = ImageBind(disable_lora=False)
 
         self.focus_videos_api = (
             #"https://dev-focus-api.omegatron.ai/"
