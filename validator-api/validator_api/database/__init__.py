0d597574550ac1cee68de469a77d4a4353f48e6a
Salman <salmanshahidzia@gmail.com>
2024-10-09 17:57:12 +0000

    engine conns bumped

--- validator-api/validator_api/database/__init__.py
+++ validator-api/validator_api/database/__init__.py
@@ -13,7 +13,14 @@ DB_PORT = config.FOCUS_DB_PORT
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=20,  # bumped up from default of 5
+    max_overflow=30,  # bumped up from default of 10
+    pool_timeout=15,  # bumped down from default of 30
+    pool_pre_ping=True,  # Good practice for most scenarios
+    pool_recycle=3600,  # Recycle connections after 1 hour
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 metadata = MetaData()
