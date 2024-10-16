9f5a7e18d0f955dbf17d24db1ae2437828a5660c
Xzistance <xzistance3@gmail.com>
2024-10-16 18:26:00 +0000

    purchase_focus_video.py

--- omega/unstuff.py
+++ omega/unstuff.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import pipeline
 from typing import Tuple
+import random
 
 UNSTUFF = pipeline("text-classification", "jondurbin/unstuffer-v0.2", device="cuda" if torch.cuda.is_available() else "cpu")
 
@@ -8,8 +9,8 @@ def is_stuffed(description: str) -> Tuple[bool, float]:
     result = UNSTUFF(description, truncation=True, max_length=512)
     stuffed = False if int(result[0]["label"]) == 1 else True
     confidence = result[0]["score"]
-    if stuffed:
+    if stuffed and confidence > 0.75:
         print(f"Detected stuffed description [{confidence=}]: {description}")
-    else:
+    elif not stuffed and random.random() <= 0.01:
         print(f"Description does not appear to be stuffed [{confidence=}]: {description}")
     return stuffed, confidence
