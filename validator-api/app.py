472fec8b362b5a86ea9517a6f6f45734a24a164a
Jonathan Chiang <jchiangconan@gmail.com>
2025-03-04 18:00:30 -0500

    cleanup and test

--- validator-api/app.py
+++ validator-api/app.py
@@ -37,7 +37,6 @@ from validator_api.config import (API_KEY_NAME, API_KEYS, COMMUNE_NETUID,
                                   FOCUS_API_URL, FOCUS_REWARDS_PERCENT,
                                   IMPORT_SCORE, IS_PROD, NETUID, NETWORK, PORT,
                                   PROXY_LIST, SENTRY_DSN)
-# from validator_api.utils.marketplace import get_max_focus_tao, get_purchase_max_focus_tao
 from validator_api.cron.confirm_purchase import (confirm_transfer,
                                                  confirm_video_purchased)
 from validator_api.database import get_db, get_db_context
@@ -802,41 +801,6 @@ async def main():
     @app.get('/api/focus/get_rewards_percent')
     async def get_rewards_percent():
         return FOCUS_REWARDS_PERCENT
-
-    # @app.get('/api/focus/get_max_focus_tao')
-    # async def _get_max_focus_tao() -> float:
-    #     return await get_max_focus_tao()
-
-    # @app.get('/api/focus/get_purchase_max_focus_tao')
-    # async def _get_purchase_max_focus_tao() -> float:
-    #     return await get_purchase_max_focus_tao()
-
-    # async def cache_max_focus_tao():
-    #     while True:
-    #         """Re-caches the value of max_focus_tao."""
-    #         print("cache_max_focus_tao()")
-
-    #         max_attempts = 3
-    #         attempt = 0
-
-    #         while attempt < max_attempts:
-    #             try:
-    #                 max_focus_tao = await get_max_focus_tao()
-    #                 break  # Exit the loop if the function succeeds
-
-    #             # In case of unforeseen errors, the api will log the error and continue operations.
-    #             except Exception as err:
-    #                 attempt += 1
-    #                 print(
-    #                     f"Error during recaching of max_focus_tao (Attempt {attempt}/{max_attempts}):", str(err))
-
-    #                 if attempt >= max_attempts:
-    #                     print(
-    #                         "Max attempts reached. Skipping this caching this cycle.")
-    #                     break
-
-    #         # Sleep in seconds
-    #         await asyncio.sleep(1800)  # 30 minutes
 
     @app.get('/api/focus/get_max_focus_alpha')
     async def _get_max_focus_alpha() -> float:
